03ddb03ad6a6c136e85521cba4c9f192e13fcdf0
JungSeungGeun <wjd15sheep@naver.com>
2023-07-09 14:57:56 +0900

    페이지 나누기

--- pages/urls.py
+++ pages/urls.py
@@ -19,9 +19,26 @@ from django.urls import path
 
 from . import views
 
+#127.0.0.1:8000                               mainpage
+#127.0.0.1:8000/productlist                   제품리스트
+#127.0.0.1:8000/productlist/detail<id>        제품상세설명
+#127.0.0.1:8000/brand                         브랜드소개
+#127.0.0.1:8000/brand/ph              브랜드철학
+#127.0.0.1:8000/brand/cs                      브랜드사회공헌
+#127.0.0.1:8000/login                         로그인
+#127.0.0.1:8000/login/signup                  회원가입
+#127.0.0.1:8000/login/password_reset          비번찾기
+
 
 urlpatterns = [
     path('', views.mainpage),
     path('company/', views.company),
-
+    path('productlist/', views.productlist),
+    path('productlist/detail/', views.productlist_detail),
+    path('brand/', views.brand),
+    path('brand/ph/', views.brand_ph),
+    path('brand/cs/', views.brand_cs),
+    path('login/', views.login),
+    path('login/signup/', views.login_signup),
+    path('login/password_reset/', views.login_password_reset),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,23 @@ def mainpage(request):
 
 def company(request):
     return render(request, 'pages/company_info.html')
+
+def productlist(request):
+    return render(request, 'pages/productlist.html')
+
+def productlist_detail(request):
+    return render(request, 'pages/product_detail.html')
+
+def brand(request):
+    return render(request, 'pages/brand_info.html')
+def brand_ph(request):
+    return render(request, 'pages/brand_ph.html')
+def brand_cs(request):
+    return render(request, 'pages/brand_cs.html')
+def login(request):
+    return render(request, 'pages/login.html')
+def login_signup(request):
+    return render(request, 'pages/signup.html')
+
+def login_password_reset(request):
+    return render(request, 'pages/login_password_reset.html')
