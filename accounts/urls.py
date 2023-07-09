309513f2dae5d4e3a79c704e94cae43bbf46d75f
JungSeungGeun <wjd15sheep@naver.com>
2023-07-10 03:06:55 +0900

    제품 디자인, 브랜드 소개, 철학, 사회공헌 디자인

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mysite/', include('mysite.urls')),
-    path('', include('pages.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('login/', LoginView.as_view(template_name="accounts/login.html")),
 ]
--- my_project/settings.py
+++ my_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
     'mysite',
     'pages',
+    'accounts',
 
 ]
 
